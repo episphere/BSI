dcde35cd8476ce255270f350fe115bd381bd2ad5
Shen <shenbn@nci-02134517-ml.fios-router.home>
2020-06-08 11:36:18 -0400

    made changes for google cloud function

--- testing.js
+++ testing.js
@@ -5,8 +5,6 @@ const http = require('http')
 
 const admin = require('firebase-admin');
 
-let serviceAccount = require('../nih-nci-dceg-episphere-dev-76b389f234a6.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
@@ -16,12 +14,20 @@ async function getEmails() {
 
   
   let db = admin.firestore();
-  let emails = [];
+  let emails = {};
   let refs = db.collection('BSIUsers');
   let allUsers = await refs.get()
   allUsers.forEach(doc => {
     let currData = doc.data()
-    if (currData.hasOwnProperty('email') && !emails.includes(currData.email)) {
+    if (currData.hasOwnProperty('email') && !emails.hasOwnProperty(currData.email)) {
+      if(currData.hasOwnProperty('role')){
+        emails[currData.email] = currData.role
+      }
+      //default is reader
+      else{
+        emails[currData.email] = 'reader'
+      }
+      
       emails.push(currData.email);
     }
     //console.log(doc.id, '=>', doc.data());
@@ -31,13 +37,8 @@ async function getEmails() {
 }
 
 
+exports.testing = (req, res) => {
 
-const hostname = '127.0.0.1';
-
-const server = http.createServer((req, res) => {
-  let emails = getEmails();
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   if (req.method === 'POST') {
     let body = [];
     req.on('data', (chunk) => {
@@ -51,6 +52,12 @@ const server = http.createServer((req, res) => {
         admin.auth().verifyIdToken(currId)
         .then(function(decodedToken) {
           console.log(decodedToken)
+          let currEmail = decodedToken.email;
+          let allEmails = getEmails()
+          .then((emails) => {
+            console.log(emails)
+          })
+          console.log(allEmails)
         }).catch(function(error) {
           
         });
@@ -59,11 +66,63 @@ const server = http.createServer((req, res) => {
     });
   }
   else {
+    
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 
   }
+
+}
+
+
+
+
+
+
+const hostname = '127.0.0.1';
+
+const server = http.createServer(async (req, res) => {
+
+  
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  console.log(req.method)
+  /*
+  if (req.method === 'POST') {
+    let body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString();
+      console.log(body)
+      let currJSON = JSON.parse(body);
+      if(currJSON.hasOwnProperty('id')){
+        let currId = currJSON['id'];
+        admin.auth().verifyIdToken(currId)
+        .then(function(decodedToken) {
+          console.log(decodedToken)
+          let currEmail = decodedToken.email;
+          let allEmails = getEmails()
+          .then((emails) => {
+            console.log(emails)
+          })
+          console.log(allEmails)
+        }).catch(function(error) {
+          
+        });
+      }
+      res.end(body);
+    });
+  }
+  else {*/
+    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+
+  //}
 
 });
 
