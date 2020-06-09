41c4f9f433a7eeb2da102d654475d9dbd57408ad
Shen <shenbn@nci-02134517-ml.nih.gov>
2020-06-09 13:25:39 -0400

    changing this so that we can use it as a google cloud repository for the function

--- function.js
+++ function.js
@@ -1,18 +1,14 @@
-//console.log('index.js loaded')
-const port = 8000;
-const fs = require('fs');
-const http = require('http')
-
-const admin = require('firebase-admin');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+let admin = require('firebase-admin');
+admin.initializeApp();
 
 async function getEmails() {
 
-  
   let db = admin.firestore();
   let emails = {};
   let refs = db.collection('BSIUsers');
@@ -27,105 +23,72 @@ async function getEmails() {
       else{
         emails[currData.email] = 'reader'
       }
-      
-      emails.push(currData.email);
     }
     //console.log(doc.id, '=>', doc.data());
   });
-  console.log(emails)
   return emails
 }
 
+exports.authenticationTesting = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  if (req.method === 'GET' || req.method === 'POST') {
+        let token = ''
+        if (req.method === 'GET') {
+            token = req.headers.authorization.substring(6)
+        }
+        else {
+            token = req.body.headers.authorization.substring(6)
+        }
+        admin.auth().verifyIdToken(token)
+            .then(function (decodedToken) {
+                getEmails()
+                    .then(function (emails) {
+                        console.log(emails)
+                        console.log(decodedToken.email)
+                        if (emails.hasOwnProperty(decodedToken.email)) {
+                            console.log(emails[decodedToken.email])
+                            if(req.hasOwnProperty('body') && req.body.hasOwnProperty('callType')){
+                              if(req.body.callType == 'addUser'){
+                                if(emails[decodedToken.email] == "admin"){
+                                  let db = admin.firestore();
+                                  db.collection('BSIUsers').add({
+                                    email:req.body.email,
+                                    name:req.body.name,
+                                    role:req.body.role
+                                  })
+                                  res.end('user added!');
+                                }
+                                else{
+                                  res.end('failed!');
+                                }
+                              }
+                            }
+                            res.end(JSON.stringify({ 'role': emails[decodedToken.email] }));
 
-exports.testing = (req, res) => {
+                        }
+                        else {
+                            res.end(JSON.stringify({ 'role': 'Sorry you do not have access to this!' }))
+                        }
+                    }).catch(function (error) {
+                        //console.log(error)
+                        res.end(JSON.stringify({ 'ERROR': error }))
+                    })
 
-  if (req.method === 'POST') {
-    let body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      body = Buffer.concat(body).toString();
-      console.log(body)
-      let currJSON = JSON.parse(body);
-      if(currJSON.hasOwnProperty('id')){
-        let currId = currJSON['id'];
-        admin.auth().verifyIdToken(currId)
-        .then(function(decodedToken) {
-          console.log(decodedToken)
-          let currEmail = decodedToken.email;
-          let allEmails = getEmails()
-          .then((emails) => {
-            console.log(emails)
-          })
-          console.log(allEmails)
-        }).catch(function(error) {
-          
-        });
-      }
-      res.end(body);
-    });
-  }
-  else {
+            }).catch(function (error) {
+                // Handle error
+                console.log(error)
+                res.end(JSON.stringify({ 'ERROR': error }))
+            });
+
+    }
+
+    else {
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(req.method);
+
+    }
     
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-
-  }
-
-}
-
-
-
-
-
-
-const hostname = '127.0.0.1';
-
-const server = http.createServer(async (req, res) => {
-
-  
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  console.log(req.method)
-  /*
-  if (req.method === 'POST') {
-    let body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      body = Buffer.concat(body).toString();
-      console.log(body)
-      let currJSON = JSON.parse(body);
-      if(currJSON.hasOwnProperty('id')){
-        let currId = currJSON['id'];
-        admin.auth().verifyIdToken(currId)
-        .then(function(decodedToken) {
-          console.log(decodedToken)
-          let currEmail = decodedToken.email;
-          let allEmails = getEmails()
-          .then((emails) => {
-            console.log(emails)
-          })
-          console.log(allEmails)
-        }).catch(function(error) {
-          
-        });
-      }
-      res.end(body);
-    });
-  }
-  else {*/
-    
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-
-  //}
-
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+};
