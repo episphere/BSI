4ff944fd32f7c4c5fc175b8b5b9268259f7b33e9
Shen <shenbn@nci-02134517-ml.nih.gov>
2020-06-11 16:19:12 -0400

    tried using the UAT

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function processCall(req,res,role, emails){
     res.end(JSON.stringify({'role':role}))
   }
   else if(url == "/bsiLogon"){
-    fetch('https://rest.bsisystems.com/api/rest/NCI/common/logon', { method: 'POST', user_name: process.env.username, password:process.env.password})
+    fetch('https://rest-uat.bsisystems.com/api/rest/NCI/common/logon', { method: 'POST', user_name: process.env.username, password:process.env.password})
     .then(response => response.json())
     .then(data => {
       res.end(JSON.stringify({'response':JSON.stringify(data)}))
